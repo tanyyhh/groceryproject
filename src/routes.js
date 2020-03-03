244dbae0be5c725fef16f7765f5db6447db9eaf3
yiheng <yiheng.tan@u.nus.edu>
2020-03-03 22:36:29 +0800

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import PageContent from './components/PageContent.vue'
+import AddItem from './components/AddItem.vue'
+
+export default [{
+        path: '/',
+        component: PageContent
+    },
+    {
+        path: '/add',
+        component: AddItem
+    }
+]
